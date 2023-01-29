ca5ebcfc1cc452a34367c6ae2cb549cbd7912123
AmirHossein-nr <arazlighi@gmail.com>
2023-01-29 17:02:38 +0330

    add "Show all passengers" view

--- frontend/AirLineSurveys/AirLineSurveys/View/ManagerView.swift
+++ frontend/AirLineSurveys/AirLineSurveys/View/ManagerView.swift
@@ -35,61 +35,71 @@ struct ManagerView: View {
                 .opacity(0.5)
                 .ignoresSafeArea()
             
-            Form {
-                // change background color of VStack
-                VStack {
-                    
-                    HStack {
-                        Text("Ticket Number: ")
-                        TextField("ticket number", text: $ticketNumber)
-                    }
-                    
-                    HStack {
-                        Text("Flight Number:")
-                        TextField("flight number", text: $flightNumber)
-                    }
-                    
-                    HStack {
-                        Text("Seat number: ")
-                        TextField("seat number", value: $seatNumber, formatter: NumberFormatter())
-                    }
-                    
-                    
-                    HStack {
-                        Text("Price: ")
-                        TextField("Price", value: $price, formatter: NumberFormatter())
+            NavigationView {
+                Form {
+                    Section(header: Text("Add new passenger")) {
+                        VStack {
+                            HStack {
+                                Text("Ticket Number: ")
+                                TextField("ticket number", text: $ticketNumber)
+                            }
+                            
+                            HStack {
+                                Text("Flight Number:")
+                                TextField("flight number", text: $flightNumber)
+                            }
+                            
+                            HStack {
+                                Text("Seat number: ")
+                                TextField("seat number", value: $seatNumber, formatter: NumberFormatter())
+                            }
+                            
+                            
+                            HStack {
+                                Text("Price: ")
+                                TextField("Price", value: $price, formatter: NumberFormatter())
+                            }
+
+                           
+                            Picker("Voter Type: ", selection: $voterType) {
+                                        Text(VOTER_TYPE.ECONOMY.rawValue).tag(VOTER_TYPE.ECONOMY)
+                                        Text(VOTER_TYPE.BUSINESS.rawValue).tag(VOTER_TYPE.BUSINESS)
+                                        Text(VOTER_TYPE.ALL.rawValue).tag(VOTER_TYPE.ALL)
+                                }
+                            
+                            HStack {
+                                Text("Enter first name: ")
+                                TextField("First Name", text: $firstName)
+                            }
+                            
+                            HStack {
+                                Text("Enter last name: ")
+                                TextField("Last Name", text: $lastName)
+                            }
+                            HStack {
+                                Text("Passport Number:")
+                                TextField("Pass num. ", text: $passportNumber)
+                            }
+
+                            Picker("Gender: ", selection: $gender) {
+                                    Text(GENDER.MALE.rawValue).tag(GENDER.MALE)
+                                    Text(GENDER.FEMALE.rawValue).tag(GENDER.FEMALE)
+                                }
+                        }
                     }
 
-                    
-                    Picker("Voter Type: ", selection: $voterType) {
-                                Text(VOTER_TYPE.ECONOMY.rawValue).tag(VOTER_TYPE.ECONOMY)
-                                Text(VOTER_TYPE.BUSINESS.rawValue).tag(VOTER_TYPE.BUSINESS)
-                                Text(VOTER_TYPE.ALL.rawValue).tag(VOTER_TYPE.ALL)
+                    Section(header: Text("Passengers")) {
+                        NavigationLink("Show all passengers") {
+                            EmptyView()
                         }
-                    
-                    HStack {
-                        Text("Enter first name: ")
-                        TextField("First Name: ", text: $firstName)
-                    }
-                    
-                    HStack {
-                        Text("Enter last name: ")
-                        TextField("Last Name: ", text: $lastName)
-                    }
-                    HStack {
-                        Text("Passport Number:")
-                        TextField("Pass num. ", text: $passportNumber)
                     }
 
-                    Picker("Gender: ", selection: $gender) {
-                            Text(GENDER.MALE.rawValue).tag(GENDER.MALE)
-                            Text(GENDER.FEMALE.rawValue).tag(GENDER.FEMALE)
-                        }
                 }
+            }
+           
             }
         }
     }
-}
 
 struct ManagerView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ frontend/AirLineSurveys/AirLineSurveys/View/ShowAllPassengers.swift
@@ -0,0 +1,20 @@
+//
+//  ShowAllPassengers.swift
+//  AirLineSurveys
+//
+//  Created by Amirhossein Razlighi on 29/01/2023.
+//
+
+import SwiftUI
+
+struct ShowAllPassengers: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowAllPassengers_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowAllPassengers()
+    }
+}
